b1bdf7a248b94691cee01efbbd974f92bb2ea971
wiseroh <wiseroh@naver.com>
2012-09-29 23:41:34 +0900

    홈메뉴 레이아웃

---
+++ src/kr/co/bh/HomeActivity.java
@@ -0,0 +1,27 @@
+package kr.co.bh;
+
+import android.os.Bundle;
+
+/**
+ * <h1>인트로 엑티비티</h1>
+ * 앱 첫 실행 엑티비티로 서버와 연결 상태 체크후
+ *	로그인 엑티비티로 이동한다. 
+ * @author 남주완
+ *
+ */
+public class HomeActivity extends BaseActivity{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_layout);
+        
+    
+    
+    
+    
+    
+    }
+        
+
+}

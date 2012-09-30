4152ebd1b8c2e0d5ea42d2adaac87e6b548e0829
wiseroh <wiseroh@naver.com>
2012-10-01 01:58:10 +0900

    홈메뉴 엑티비스 스타일 수량 체크 엑티비티 이동

--- src/kr/co/bh/HomeActivity.java
+++ src/kr/co/bh/HomeActivity.java
@@ -1,12 +1,13 @@
 package kr.co.bh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
- * <h1>인트로 엑티비티</h1>
- * 앱 첫 실행 엑티비티로 서버와 연결 상태 체크후
- *	로그인 엑티비티로 이동한다. 
- * @author 남주완
+ * <h1>홈메뉴 엑티비티</h1>
+ * 홈메뉴
+ * @author 노병원
  *
  */
 public class HomeActivity extends BaseActivity{
@@ -15,13 +16,19 @@ public class HomeActivity extends BaseActivity{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
-        
-    
-    
-    
     
     
     }
         
-
+    //스타일 수량 체크 엑티비티로 이동한다
+	public void StyleActivityGo(View v) {
+		try {
+			Intent intent = new Intent(this, StyleActivity.class);
+			/*intent.putExtra("key","value");  해당 엑티비티에 넘길 데이커가 있을 경우 */
+			startActivity(intent);
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+    
 }
